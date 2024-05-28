9770a2cc3a9e4a3f0b9b11084b8747f49086eb85
Katya Hernández <katya0112>
2024-05-27 19:38:14 -0500

    Efectos a tarjetas de portafolio

--- Javascript/expansionImagen.js
+++ Javascript/expansionImagen.js
@@ -1,4 +1,4 @@
-const cards = document.querySelectorAll('.card img'); // Select all images inside cards
+const cards = document.querySelectorAll('.card img'); // Selecciona todas las imágenes dentro de las tarjetas.
 const modal = document.querySelector('.modal');
 const modalImage = modal.querySelector('.modal-content');
 const closeButton = modal.querySelector('.close');
@@ -7,16 +7,16 @@ cards.forEach(cardImage => {
   cardImage.addEventListener('click', () => {
     const imageSrc = cardImage.src;
     modalImage.src = imageSrc;
-    modal.style.display = 'block'; // Show the modal
+    modal.style.display = 'block'; // Mostrar el modal
   });
 });
 
 closeButton.addEventListener('click', () => {
-  modal.style.display = 'none'; // Hide the modal
+  modal.style.display = 'none'; // Ocultar el modal
 });
 
 window.addEventListener('click', (event) => {
-  if (event.target === modal) { // Close modal by clicking outside
+  if (event.target === modal) { //Cerrar modal haciendo clic fuera
     modal.style.display = 'none';
   }
 });
